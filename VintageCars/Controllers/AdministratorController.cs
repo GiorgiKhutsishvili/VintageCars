40548879f69c37c2b5d20b33ce533775e293640f
GiorgiKhutsishvili <34691385+GiorgiKhutsishvili@users.noreply.github.com>
2017-12-28 17:37:56 +0400

    changes in project

---
+++ VintageCars/Controllers/AdministratorController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VintageCars.Models;
+
+namespace VintageCars.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        // GET: Administrator
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Login(Login login)
+        {
+            return View();
+        }
+
+
+        public ActionResult Adminpanel()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Adminpanel()
+        //{
+        //    return View();
+        //}
+
+    }
+}
---
+++ VintageCars/Models/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace VintageCars.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "ჩაწერეთ ელ.ფოსტა")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(50, ErrorMessage = "ჩაწერილი ელ.ფოსტა არ უნდა აღემატებოდეს 50 სიმბოლოს")]
+        [EmailAddress(ErrorMessage = "გთხოვთ ჩაწეროთ სწორედ ელ.ფოსტის მისამართ")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                            @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                            @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "ელ.ფოსტის ფორმატი არასწორია.")]
+
+        public string Email { get; set; }
+
+        
+        [Required(ErrorMessage = "ჩაწერეთ პაროლი")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
