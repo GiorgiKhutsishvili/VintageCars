eab6f55747d861167cdcd4273f95b56708e5c326
GiorgiKhutsishvili <34691385+GiorgiKhutsishvili@users.noreply.github.com>
2017-12-29 17:43:59 +0400

    changes

--- VintageCars/Controllers/AdministratorController.cs
+++ VintageCars/Controllers/AdministratorController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,16 +10,40 @@ namespace VintageCars.Controllers
 {
     public class AdministratorController : Controller
     {
-        // GET: Administrator
+
+        VintageCarsDBEntities db = new VintageCarsDBEntities();
+        
+
         public ActionResult Login()
         {
+            if (Session["admin"] != null)
+            {
+                return RedirectToAction("Adminpanel", "Administrator");
+            }
             return View();
         }
         
         [HttpPost]
         public ActionResult Login(Login login)
         {
-            return View();
+            AdminTbl admin = db.AdminTbls.FirstOrDefault(x => x.Email == login.Email && x.Password == login.Password); 
+            
+            if(admin == null)
+            {
+                ViewBag.message = "მომხმარებლის სახელი ან პაროლი არასწორია";
+                return View();
+            }
+            else
+            {
+                Session["admin"] = admin;
+                return RedirectToAction("Adminpanel", "Administrator");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login", "Administrator");
         }
 
 
@@ -27,11 +52,49 @@ namespace VintageCars.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public ActionResult Adminpanel()
-        //{
-        //    return View();
-        //}
+
+        public ActionResult Addimage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Addimage(ImageValidation image, FormCollection fc, HttpPostedFileBase file)
+        {
+            ImageTbl tbl = new ImageTbl();
+
+            var allowedExtensions = new[] {
+            ".Jpg", ".png", ".jpg", "jpeg"
+            };
+
+            tbl.Id = image.Id;
+            tbl.Image_url = file.ToString();
+            tbl.Title = image.Title;
+
+            var fileName = Path.GetFileName(file.FileName); //getting only file name(ex-img.jpg)
+            var ext = Path.GetExtension(file.FileName); //getting the extension(ex-.jpg)
+
+            if (allowedExtensions.Contains(ext)) //check what type of extension  
+            {
+                string name = Path.GetFileNameWithoutExtension(fileName); //getting file name without extension
+                string myfile = name + "_" + tbl.Id + ext; //appending the name with id
+
+                // store the file inside ~/project folder(Img)
+                var path = Path.Combine(Server.MapPath("~/Img"), myfile);
+                tbl.Image_url = path;
+                tbl.CarPicture = name;
+                tbl.Date = DateTime.Now;
+                db.ImageTbls.Add(tbl);
+                db.SaveChanges();
+                file.SaveAs(path);
+            }
+            else
+            {
+                ViewBag.message = "Please choose only Image file";
+            }
+
+            return View();
+        }
 
     }
 }
---
+++ VintageCars/Models/ImageValidation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace VintageCars.Models
+{
+    public class ImageValidation
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "აირჩიეთ ფაილი")]
+        public HttpPostedFileBase file { get; set; }
+
+        [Required(ErrorMessage = "დასათაურება")]
+        public string Title { get; set; }
+    }
+}
